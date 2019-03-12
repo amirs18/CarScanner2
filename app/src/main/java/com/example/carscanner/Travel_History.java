a7961b2fb6d6e1e13623708ef87942eb4da19dae
amir shafat <amir.shafat1@gmail.com>
2019-03-12 11:25:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/carscanner/Travel_History.java
@@ -0,0 +1,15 @@
+package com.example.carscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Travel_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel__history);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/carscanner/Trip.java
@@ -0,0 +1,13 @@
+package com.example.carscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip);
+    }
+}
